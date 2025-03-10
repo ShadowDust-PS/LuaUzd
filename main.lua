c6a3f6e7ed58dd47170a3462161d6de9fcf8c9a5
Vilimas <vilimasbutkus321@gmail.com>
2025-03-10 17:35:42 +0200

    pagrindine meniu logika

---
+++ main.lua
@@ -0,0 +1,41 @@
+local WeatherAPI = require('weather_api')
+local WeatherData = require('weather_data')
+local FavoriteCities = require('fav_city')
+
+local api = WeatherAPI:new()
+local db = FavoriteCities:new()
+
+while true do
+    print('\nPasirinkite veiksmą: ')
+    print('\n1. Gauti orus pagal miestą')
+    print('\n2. Pridėti miestą į mėgstamus')
+    print('\n3. peržiūrėti mėgstamus miestus')
+    print('\n4. Išeiti')
+    
+    local choice = io.read()
+    if choice == '1' then
+        print('Įveskite miesto pavadinimą: ')
+        local city = io.read()
+        local status, data = pcall(api.getWBC, api, city)
+        if status then
+            local weather = WeatherData:new(data)
+            weather:disp()
+        else
+            print('Klaida gaunant orų duomenis:', data)
+        end
+
+    elseif choice == '2' then
+        print('Įveskite mėgstamo miesto pavadinimą: ')
+        pcall(db.addcity, db, city)
+
+    elseif choice == '3' then
+        print('Mėgstami miestai: ')
+        pcall(db.getcities, db)
+
+    elseif choice == '4' then
+        print('Programa baigiama.')
+        break
+    else
+        print('Neteisingas pasirinkimas, bandykite dar kartą.')
+    end
+end
